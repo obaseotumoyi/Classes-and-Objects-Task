f2299ea70b1fb9134b75bfbffc1f99f013644756
Ekor Ibor <47329386+obaseotumoyi@users.noreply.github.com>
2022-05-28 23:11:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# Classes and Objects
+
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = int(age)
+        self.tracks = tracks
+        self.score = float(score)
+
+    # Create methods for student class
+
+    def change_name(self, name):
+      print("My name is " + self.name)
+
+    def change_age(self, age):
+      print(int(self.age))
+
+    def add_track(self, tracks):
+      print(self.tracks)
+
+    def get_score(self):
+      print(self.score)
+
+# Object
+
+Bob = Student("Bob", 26, ["Full Time","Part Time"], 20.90)
+
+
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+
